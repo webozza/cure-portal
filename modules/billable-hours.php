6a458b9b3f832f38f353a0fc833695d814930a56
webozza <webozza@gmail.com>
2023-04-11 13:11:44 +0800

    version update

--- modules/billable-hours.php
+++ modules/billable-hours.php
@@ -4,6 +4,29 @@
 <!-- HEAD -->
 <?php include($curepath . 'header.php') ?>
 
+<!-- Auth -->
+<?php
+
+    $curl = curl_init();
+
+    curl_setopt_array($curl, array(
+    CURLOPT_URL => 'https://api.timelyapp.com/1.1/oauth/authorize?response_type=code%0A&redirect_uri=urn:ietf:wg:oauth:2.0:oob&client_id=pRVYnxXWTFYlo92q1Pw0PrmosVjg0UoLoABZoZXYe0s%0A%0A',
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING => '',
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 0,
+    CURLOPT_FOLLOWLOCATION => true,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => 'GET',
+    ));
+
+    $response = curl_exec($curl);
+
+    curl_close($curl);
+    echo $response;
+    
+?>
+
 <!-- BODY -->
 <body class="billable-hours">
     
@@ -13,7 +36,7 @@
         <!-- Panel -->
         <div class="panel">
             <div class="panel-head">
-                <h1>Checklists</h1>
+                <h1>Billable Hours</h1>
             </div>
         </div>
     </div>
