e6fb7df602e05d7de69e8a1485b21f83f3105be5
webozza <webozza@gmail.com>
2023-04-11 14:25:03 +0800

    testing response

--- modules/billable-hours.php
+++ modules/billable-hours.php
@@ -13,34 +13,28 @@
 
 <!-- Auth with Timely -->
 <?php if( isset($_GET['code']) ) {
-    echo $_GET['code'];
-    echo 'fasdfasdf';
-} else {
-    echo 'else';
+
+    // GET TOKEN
+    $response_code = $_GET['code'];
+    $curl = curl_init();
+
+    curl_setopt_array($curl, array(
+    CURLOPT_URL => 'https://api.timelyapp.com/1.1/oauth/token?client_id=pRVYnxXWTFYlo92q1Pw0PrmosVjg0UoLoABZoZXYe0s&client_secret=EQZi9DwhIQHi6W6gPSklmLnAqXY9NhmR1BUhcvWPiCk&redirect_uri=https://checklist.curedev.com.au/skel/modules/billable-hours.php&code='.$response_code.'&grant_type=authorization_code',
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING => '',
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 0,
+    CURLOPT_FOLLOWLOCATION => true,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => 'POST',
+    ));
+
+    $response = curl_exec($curl);
+
+    curl_close($curl);
+    echo $response;
+
 } ?>
-
-
-<?php
-    
-    
-    // $curl = curl_init();
-
-    // curl_setopt_array($curl, array(
-    // CURLOPT_URL => 'https://api.timelyapp.com/1.1/oauth/token?client_id=pRVYnxXWTFYlo92q1Pw0PrmosVjg0UoLoABZoZXYe0s&client_secret=EQZi9DwhIQHi6W6gPSklmLnAqXY9NhmR1BUhcvWPiCk&redirect_uri=https://checklist.curedev.com.au/skel/modules/billable-hours.php&code=vxJ5XisbpBZ1NjoyQLKwj5vKVkzYvhOIqo5xtjyGNGU&grant_type=authorization_code',
-    // CURLOPT_RETURNTRANSFER => true,
-    // CURLOPT_ENCODING => '',
-    // CURLOPT_MAXREDIRS => 10,
-    // CURLOPT_TIMEOUT => 0,
-    // CURLOPT_FOLLOWLOCATION => true,
-    // CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-    // CURLOPT_CUSTOMREQUEST => 'POST',
-    // ));
-
-    // $response = curl_exec($curl);
-
-    // curl_close($curl);
-    // echo $response;
-?>
 
 <!-- BODY -->
 <body class="billable-hours" style="display:none;">
