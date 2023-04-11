b1d9bc401314f1171871c5c479cc8aa88a9e1ceb
webozza <webozza@gmail.com>
2023-04-11 13:47:38 +0800

    version update

--- modules/billable-hours.php
+++ modules/billable-hours.php
@@ -27,6 +27,28 @@
     curl_close($curl);
     echo $response;
     
+    if( isset($_GET['code']) ) {
+
+        echo $_GET['code'];
+
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+        CURLOPT_URL => 'https://api.timelyapp.com/1.1/oauth/token?redirect_uri=urn:ietf:wg:oauth:2.0:oob&code=khEvbirQ_FV49uU5qcqehrsubSO6fw5WfhWqKNCtrGU&client_id=pRVYnxXWTFYlo92q1Pw0PrmosVjg0UoLoABZoZXYe0s&client_secret=EQZi9DwhIQHi6W6gPSklmLnAqXY9NhmR1BUhcvWPiCk&grant_type=hQaToT1fbyQFe9JDp4qrZe9nHzCAQZtLI45-UsIeD5Y',
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => '',
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => 'POST',
+        ));
+
+        $response = curl_exec($curl);
+
+        curl_close($curl);
+        echo $response;
+    }
 ?>
 
 <!-- BODY -->
